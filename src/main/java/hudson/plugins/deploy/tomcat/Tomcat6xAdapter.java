686fa220245200e4d454371bf7e7d30f49814d6c
Guillermo Gutiérrez <guillermo.gutierrez@donewtech.com>
2011-09-07 13:13:48 +0200

    Fixed Tomcat 6.x manager path

--- src/main/java/hudson/plugins/deploy/tomcat/Tomcat6xAdapter.java
+++ src/main/java/hudson/plugins/deploy/tomcat/Tomcat6xAdapter.java
@@ -2,7 +2,12 @@ package hudson.plugins.deploy.tomcat;
 
 import hudson.Extension;
 import hudson.plugins.deploy.ContainerAdapterDescriptor;
+import org.codehaus.cargo.container.configuration.Configuration;
+import org.codehaus.cargo.container.tomcat.TomcatPropertySet;
 import org.kohsuke.stapler.DataBoundConstructor;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Tomcat 6.x
@@ -14,6 +19,16 @@ public class Tomcat6xAdapter extends TomcatAdapter {
     @DataBoundConstructor
     public Tomcat6xAdapter(String url, String password, String userName) {
         super(url, password, userName);
+    }
+
+		public void configure(Configuration config) {
+        super.configure(config);
+        try {
+            URL _url = new URL(url + "/manager/html");
+            config.setProperty(TomcatPropertySet.MANAGER_URL,_url.toExternalForm());
+        } catch (MalformedURLException e) {
+            throw new AssertionError(e);
+        }
     }
 
     public String getContainerId() {
