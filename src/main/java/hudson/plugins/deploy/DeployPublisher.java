e0bbeb0a8de7cbc0bc9ce43f29149094601b0501
Jansohn Robin <git@jansohn.org>
2019-06-19 09:06:03 +0200

    fixed missing imports

--- src/main/java/hudson/plugins/deploy/DeployPublisher.java
+++ src/main/java/hudson/plugins/deploy/DeployPublisher.java
@@ -2,6 +2,7 @@ package hudson.plugins.deploy;
 
 import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials;
 import hudson.*;
+import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.model.Result;
 import hudson.model.Run;
@@ -11,6 +12,7 @@ import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.BuildStepMonitor;
 import hudson.tasks.Notifier;
 import hudson.tasks.Publisher;
+import hudson.util.VariableResolver;
 import jenkins.model.Jenkins;
 import jenkins.tasks.SimpleBuildStep;
 import jenkins.util.io.FileBoolean;
